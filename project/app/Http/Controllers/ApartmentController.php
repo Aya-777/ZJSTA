72bd22910930d94e05d9d4f5f429c157c1cc39c1
Aya yassien <yassienaya714@gmail.com>
2025-11-28 17:11:46 +0300

    implementing basic crud methods

--- project/app/Http/Controllers/ApartmentController.php
+++ project/app/Http/Controllers/ApartmentController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
-    //
+    // index
+    public function index()
+    {
+        // Code to list all apartments
+    }
+    // show
+    public function show($id)
+    {
+        // Code to show a specific apartment
+    }
+    // create
+    public function create(){
+        // Code to show form to create a new apartment
+    }
+    // store
+    public function store(Request $request){
+        // Code to store a new apartment
+    }
+    // edit
+    public function edit($id){
+        // Code to show form to edit an apartment
+    }
+    // update
+    public function update(Request $request, $id){
+        // Code to update an apartment
+    }
+    // destroy
+    public function destroy($id){
+        // Code to delete an apartment
+    }
 }
