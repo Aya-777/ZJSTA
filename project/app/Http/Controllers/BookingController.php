83e1f706d265c258f5086cec1a1233b644162ea0
Aya yassien <yassienaya714@gmail.com>
2025-12-25 12:20:38 +0300

    update booking for apartment to accept all and reject them when owner confirms one

--- project/app/Http/Controllers/BookingController.php
+++ project/app/Http/Controllers/BookingController.php
@@ -18,7 +18,7 @@ class BookingController extends Controller
 {
   // index
     public function index(Request $request){
-      $user = User::find(1); // Temporarily hardcoded for testing use auth()->user();
+      $user = User::find(2); // Temporarily hardcoded for testing use auth()->user();
       $query = Booking::where('user_id', $user->id);
       $query = BookingFilter::apply($query ,$request);
       $bookings = $query->with(['apartment','user'])
@@ -28,7 +28,7 @@ class BookingController extends Controller
     }
     // show
     public function show(Booking $booking){
-      $user = User::find(1); // Temporarily hardcoded for testing use auth()->user();
+      $user = User::find(2); // Temporarily hardcoded for testing use auth()->user();
       if($user->id !== $booking->user_id){
           abort(404);
       }
@@ -46,16 +46,17 @@ class BookingController extends Controller
         
         $apartment = Apartment::find($validated['apartment_id']);
         $apartmentBookings = $apartment->bookings()
+          ->where('status','confirmed')
           ->where(function($query) use ($validated) {
               $query->whereBetween('start_date', [$validated['start_date'], $validated['end_date']])
                 ->orWhereBetween('end_date', [$validated['start_date'], $validated['end_date']])
                 ->orWhere(function($query) use ($validated) {
                   $query->where('start_date', '<=', $validated['start_date'])
                         ->where('end_date', '>=', $validated['end_date']);
-                  $query->where('status', 'confirmed');
                     });
           })->exists();
         if($apartmentBookings){
+
             return response()->json(['message' => 'The apartment is already booked for the selected dates.'], 409);
         } 
         
@@ -128,6 +129,20 @@ class BookingController extends Controller
         return response()->noContent();
     }
 
+
+    public function notifyRenter($renter, $title, $message){
+      // Save to DB history for Renter
+      $renter->notify(new UpdateBookingNotification($booking));
+
+      // Send Push to Renter
+      if ($renter->fcm_token) {
+        $fcm->sendNotification($renter->fcm_token, $title, $message, [
+            'booking_id' => (string)$booking->id,
+            'status' => $decision
+        ]);
+      }
+    }
+
   public function updateStatus(Request $request, $id, FcmService $fcm){      
     $request->validate(['status' => 'required|in:confirmed,rejected,completed']);
     
@@ -154,6 +169,7 @@ class BookingController extends Controller
         ]);
       }
 
+    // Notify the renter
     $renter = $booking->user;
     $title = $decision == 'confirmed' ? 'Booking Approved!' :
     ($decision == 'rejected' ? 'Booking Rejected!' :
@@ -164,16 +180,34 @@ class BookingController extends Controller
           ? "We're sorry to inform you that your booking request for {$booking->apartment->title} has been rejected."
           : "Thank you for staying at {$booking->apartment->title}! We hope you had a great time.");
 
-    // Save to DB history for Renter
-    $renter->notify(new UpdateBookingNotification($booking));
+    notifyRenter($renter, $title, $message);
 
-    // Send Push to Renter
-    if ($renter->fcm_token) {
-      $fcm->sendNotification($renter->fcm_token, $title, $message, [
-          'booking_id' => (string)$booking->id,
-          'status' => $decision
-      ]);
-    }
+
+    // Reject all overlapping pending bookings
+    $apartment = $booking->apartment;
+    $apartmentBookings = $apartment->bookings()
+      ->where('status','pending')
+      ->where(function($query) use ($booking) {
+          $query->whereBetween('start_date', [$booking->start_date , $booking->end_date])
+            ->orWhereBetween('end_date', [$booking->start_date, $booking->end_date])
+            ->orWhere(function($query) use ($booking) {
+              $query->where('start_date', '<=', $booking->start_date)
+                    ->where('end_date', '>=', $booking->end_date);
+                });
+      })->get();
+      if(!empty($apartmentBookings)){
+        foreach($apartmentBookings as $b){
+          $b->update(['status' => 'rejected']);
+
+          // Notify the renter
+          $renter = $b->user;
+          $title = 'Your Booking has been rejected.';
+          $message = 'Sorry the apartment is already booked for the selected dates( ' . $b->start_date . ' to ' . $b->end_date . ' )';
+
+          notifyRenter($renter, $title, $message);
+        }
+      }
+      
 
     return response()->json(['message' => 'Booking updated successfully', 'status' => $decision]);
   }
